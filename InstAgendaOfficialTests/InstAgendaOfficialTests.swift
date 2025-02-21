7c591871b556064057891672878c92d726bd4fe7
Derald Blesman <dblessman24@gmail.com>
2025-02-21 15:42:08 -0500

    Initial Commit

---
+++ InstAgendaOfficialTests/InstAgendaOfficialTests.swift
@@ -0,0 +1,17 @@
+//
+//  InstAgendaOfficialTests.swift
+//  InstAgendaOfficialTests
+//
+//  Created by Derald Blessman on 2/21/25.
+//
+
+import Testing
+@testable import InstAgendaOfficial
+
+struct InstAgendaOfficialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
